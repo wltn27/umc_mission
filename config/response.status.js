454257a0e764ea89d893e38bf8b5923c8617ca5d
wltn27 <@pjs082701@naver.com>
2023-12-28 11:46:24 +0900

    feature/3 API response.status.js 수정

--- config/response.status.js
+++ config/response.status.js
@@ -18,6 +18,8 @@ export const status = {
 		// member err
     MEMBER_NOT_FOUND: {status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "MEMBER4001", "message": "사용자가 없습니다."},
     NICKNAME_NOT_EXIST: {status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "MEMBER4002", "message": "닉네임은 필수입니다."},
+    EMAIL_ALREADY_EXIST: {status: StatusCodes.CONFLICT, "isSuccess": false, "code": "MEMBER4003", "message": "이미 등록된 이메일입니다."},
+    PARAMETER_IS_WRONG: {status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "MEMBER4004", "message": "파라미터 값이 잘못되었습니다."},
 
     // article err
     ARTICLE_NOT_FOUND: {status: StatusCodes.NOT_FOUND, "isSuccess": false, "code": "ARTICLE4001", "message": "게시글이 없습니다."}
